2563585128332907c64e23c78b9d1d097d3eb4bc
linachaal2 <156126149+linachaal2@users.noreply.github.com>
2024-04-12 11:03:22 +0200

    Update createRolloutPackage.pl

--- scripts/createRolloutPackage.pl
+++ scripts/createRolloutPackage.pl
@@ -1303,7 +1303,8 @@ if (!-e  $ro_dir.$ro)
 	} 
 	close($vInputFile); 
         printf("Moving $SrcInputFile\n");
-	move($SrcInputFile, "./rollout/") or die "Move failed: $!";
+	move($SrcInputFile, "/Users/runner/work/SWBYDEMO/SWBYDEMO/rollout/") or die "Move failed: $!";
+        printf("$SrcInputFile moved\n");
 } # done creating input file
 printf("Validating $SrcInputFile\n");
 #validate ro file exist
