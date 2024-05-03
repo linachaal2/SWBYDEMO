135742bc47743857abed6e9e5efe78a6b5077c3e
linachaal2 <linachaal2[bot]@users.noreply.github.com>
2024-05-03 14:27:37 -0400

    update sorting

--- scripts/helloStarware.pl
+++ scripts/helloStarware.pl
@@ -2,7 +2,29 @@
 
 # Modules used 
 use strict; 
-use warnings; 
+use warnings;
+use File::Find;
 
 # Print function 
 print("Hello World\n"); 
+
+
+
+my @content;
+my @sorted;
+#find( \&wanted, \&preprocess, '/y/Docker/tmp');
+#find( { wanted => \&wanted, preprocess => \&preprocess}, '/y/Docker/tmp');
+#find( {preprocess => \&preprocess,wanted => \&wanted,}, '/y/Docker/tmp');
+
+#find({preprocess => \&before, wanted => \&when, postprocess => \&after}, '/y/Docker/tmp');
+find({preprocess => \&before, wanted => \&when}, '/y/Docker/tmp');
+
+sub before
+  {
+  print "Sorting files ";
+  sort @_
+  }
+
+#sub after {print "Againnnnnn file! $File::Find::name "}
+
+sub when { print "file! $File::Find::name " }
