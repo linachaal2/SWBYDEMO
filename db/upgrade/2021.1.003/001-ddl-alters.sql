ef9dc4b0e3ea80fadd8f70d8ecf72713c2bf34e8
linachaal2 <linachaal2[bot]@users.noreply.github.com>
2024-06-04 11:28:44 -0400

    update

---
+++ db/upgrade/2021.1.003/001-ddl-alters.sql
@@ -0,0 +1,13 @@
+#use $DCSDIR/include
+#use $MOCADIR/include
+#use $LESDIR/include
+#include <usrddl.h>
+#include <usrcolwid.h>
+#include <usrtbldef.h>
+#include <dcsddl.h>
+#include <dcscolwid.h>
+#include <dcstbldef.h>
+#include <sqlDataTypes.h>
+
+#include "les_dataset.tbl"
+#include "les_dbversion.tbl"
